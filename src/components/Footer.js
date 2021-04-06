d811163dbb3f574596e70aa444ceeebf46834e5f
Kevin Jiménez <ji.oso.kev@gmail.com>
2021-04-06 10:59:17 -0500

    Agregando las redes sociales al footer de la aplicacion del cine

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Footer.css';
 
+import logoGithub from '../img/github_logo.png';
+import logoTwitter from '../img/twitter_logo.png';
+import logoInstagram from '../img/instagram_logo.png';
+
 class Footer extends React.Component {
   render() {
     return (
       <div className='Footer'>
-        <Link to='/'>
-          <span className='Footer--title'>PelisTendencia</span>
-        </Link>
+        <div className='Footer-container'>
+          <a href='https://github.com/KevJim' className='footer-social-github'>
+            <img src={logoGithub} className='footer--logo'></img>
+            <span>Github</span>
+          </a>
+          <a href='https://twitter.com/Jiokso' className='footer-social'>
+            <img src={logoTwitter} className='footer--logo'></img>
+            <span>Twitter</span>
+          </a>
+          <a
+            href='https://www.instagram.com/kevjimoso/'
+            className='footer-social'>
+            <img src={logoInstagram} className='footer--logo'></img>
+            <span>Instagram</span>
+          </a>
+        </div>
       </div>
     );
   }
